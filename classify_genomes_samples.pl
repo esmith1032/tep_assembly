1ef63db54899ddd9915622bb3117f29cf0c25c8d
esmith1032 <esmit013@ucr.edu>
2017-06-13 14:36:34 -0700

    Create classify_genomes_samples.pl

--- classify_genomes_samples.pl
+++ classify_genomes_samples.pl
@@ -201,9 +201,9 @@ foreach my $sample (keys %sample_allele_counts)
   
 sub prepareDbh
   {
-  my $username = "david";
-  my $password = "drosophila";
-  my $database = "tapl";
+  my $username = #REDACTED#;
+  my $password = #REDACTED#;
+  my $database = #REDACTED#;
   
   
   my $dsn = "DBI:mysql:$database:localhost";
